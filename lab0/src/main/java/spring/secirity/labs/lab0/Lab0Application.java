c7e7d217e35e7a28407f62cc2812c929e3b62686
Kirill Pisarenko <assent222@gmail.com>
2017-07-11 00:30:30 +0300

    init lab0

---
+++ lab0/src/main/java/spring/secirity/labs/lab0/Lab0Application.java
@@ -0,0 +1,12 @@
+package spring.secirity.labs.lab0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab0Application.class, args);
+	}
+}
